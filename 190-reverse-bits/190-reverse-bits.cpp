a750052aa3edda7606ce7fe390bb1006cf15b68f
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-01-05 08:39:10 +0530

    Time: 8 ms (5.58%), Space: 6 MB (50.31%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        string s = "";
         uint32_t bit = 0; uint32_t ans = 0;
         for(int i = 31; i >= 0; i--){
             if((1 << i)&n) bit = 1;
