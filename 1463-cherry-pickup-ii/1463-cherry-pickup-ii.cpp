62f0ceba082823947644620fb72423ba144a6cd1
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-01-08 15:20:36 +0530

    Time: 91 ms (30.44%), Space: 14.1 MB (45.36%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,35 +1,55 @@
 class Solution {
 public:
+    int helper(vector<vector<int>>&grid,int a, int b, int c, int d,int &r, int &cl,int ***dp)
+    {
+        // cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+        if(a>=r || c>=r || b>=cl || d>=cl || b<0 || d<0 )return 0;
+        if(a==c && b==d)return 0;
+        if(dp[a][b][d]!=-1)return dp[a][b][d];
+        int ans1 = helper(grid, a+1,b-1,c+1,d-1,r,cl,dp) + grid[a][b] + grid[c][d] ;
+        int ans2 = helper(grid, a+1,b-1,c+1,d,r,cl,dp) + grid[a][b] + grid[c][d];
+        int ans3 = helper(grid, a+1,b-1,c+1,d+1,r,cl,dp) + grid[a][b] + grid[c][d];
+        
+        int ans4 = helper(grid, a+1,b,c+1,d-1,r,cl,dp) + grid[a][b] + grid[c][d];
+        int ans5 = helper(grid, a+1,b,c+1,d,r,cl,dp) + grid[a][b] + grid[c][d];
+        int ans6 = helper(grid, a+1,b,c+1,d+1,r,cl,dp) + grid[a][b] + grid[c][d];
+        
+        int ans7 = helper(grid, a+1,b+1,c+1,d-1,r,cl,dp) + grid[a][b] + grid[c][d];
+        int ans8 = helper(grid, a+1,b+1,c+1,d,r,cl,dp) + grid[a][b] + grid[c][d];
+        int ans9 = helper(grid, a+1,b+1,c+1,d+1,r,cl,dp) + grid[a][b] + grid[c][d];
+        
+        int ans =  max({ans1,ans2,ans3,ans4,ans5,ans6,ans7,ans8,ans9});
+        dp[a][b][d] = ans;
+        return ans;
+    }
     int cherryPickup(vector<vector<int>>& grid) {
-        int m = (int)grid.size(), n = (grid[0].size());
-        //initializing m and n
-        vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n, vector<int>(n, 0)));
-        //dp dimensions
-        dp[0][0][n - 1] = 0 == n - 1 ? grid[0][0] : grid[0][0] + grid[0][n - 1];
-        //row traversing
-        for (int row = 1; row < m; row++) {
-            //avoiding unnecessary column computations
-            for (int c1 = 0; c1 < min(n, row + 1); c1++) {
-                for (int c2 = max(0, n - 1 - row); c2 < n; c2++) {
-                    int prevMax = 0;
-                    //Moves of robos in columns
-                    for (int offset1 = max(0, c1 - 1); offset1 <= min(n - 1, c1 + 1); offset1++) {
-                        for (int offset2 = max(0, c2 - 1); offset2 <= min(n - 1, c2 + 1); offset2++) {
-                                prevMax = max(prevMax, dp[row - 1][offset1][offset2]); //9 states wediscussed can be summariazed 
-                        }
-                    }
-                    //Robo collision condition
-                    if (c1 == c2) dp[row][c1][c2] = prevMax + grid[row][c1];
-                    else dp[row][c1][c2] = prevMax + grid[row][c1] + grid[row][c2];
-                }
+        
+        int r=grid.size(),c=grid[0].size();
+        // creating dp array
+        int ***dp = new int**[r+1];
+        for(int i=0;i<r+1;i++)
+        {
+            dp[i]=new int*[c+1];
+            for(int j=0;j<c+1;j++)
+            {
+                dp[i][j]=new int[c+1];
+                for(int l=0;l<c+1;l++)dp[i][j][l]=-1;
+                
             }
         }
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                res = max(res, dp[m - 1][i][j]);    
+        
+        int ans = helper(grid,0,0,0,c-1,r,c,dp);
+        // deleting dp array
+        for(int i=0;i<r+1;i++)
+        {
+            for(int j=0;j<c+1;j++)
+            {
+                delete[] dp[i][j];
             }
+            delete[] dp[i];
         }
-        return res;
+        delete[] dp;
+        return ans;
+        
     }
 };
