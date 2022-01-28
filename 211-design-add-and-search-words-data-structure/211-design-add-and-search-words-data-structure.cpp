cce525c30230e74a700801c9fa45c89c6c2ae423
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-01-28 23:35:31 +0530

    Time: 316 ms (5.25%), Space: 54.6 MB (34.75%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,71 @@
+class node{
+public:
+    char data;
+    bool isTerminal;
+    unordered_map<char, node*> children;
+    
+    node(char data){
+        this->data= data;
+        isTerminal= false;
+    }
+};
+
+class WordDictionary {
+public:
+    node* root;
+    int count;
+    
+    WordDictionary() {
+        this->root= new node('\0');
+        count=0;
+    }
+    
+    bool helper(string word, node* curr){
+        int n= word.size();
+        
+        for(int i=0; i<n; i++){
+            char ch= word[i];
+            
+            if(ch=='.'){
+                for(char ch='a'; ch<='z'; ch++){
+                    if(curr->children[ch]){
+                        if(helper(word.substr(i+1), curr->children[ch]))
+                            return true;
+                    }
+                }
+                return false;
+            }
+            else{
+                if(curr->children[ch]==NULL)
+                    return false;
+                curr= curr->children[ch];
+            }
+        }
+        if(curr and curr->isTerminal)
+            return true;
+        return false;
+        }
+        
+    
+    void addWord(string word) {
+        node* curr= root;
+        int n= word.size();
+        for(int i=0; i<n; i++){
+            char ch= word[i];
+            if(curr->children[ch]){
+                curr= curr->children[ch];
+            }
+            else{
+                node* t= new node(ch);
+                curr->children[ch]= t;
+                curr= t;
+            }
+        }
+        curr->isTerminal= true;
+        count++;
+    }
+    
+    bool search(string word) {
+        return helper(word, root);
+    }
+};
