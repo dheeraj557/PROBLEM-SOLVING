69c77085162705d3483912223810d1ce4f6fb10d
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-02-02 09:41:23 +0530

    Time: 36 ms (33.61%), Space: 8.6 MB (90.96%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int p_len = p.size(), s_len = s.size();
+        
+        if (p_len > s_len) {
+            return {};
+        }
+        
+        vector<int> p_freq(26, 0), s_freq(26, 0), sol;
+        
+        for (int i = 0; i < p_len; i++) {
+            p_freq[p[i] - 'a']++;
+            s_freq[s[i] - 'a']++;
+        }
+        
+        if (p_freq == s_freq) {
+            sol.push_back(0);  
+        } 
+
+        for (int i = 1; i < s_len - p_len + 1; i++) {
+            s_freq[s[i - 1] - 'a']--;
+            s_freq[s[i + p_len - 1] - 'a']++;
+            if (s_freq == p_freq) {
+                sol.push_back(i);
+            }
+        }
+        
+        return sol;
+    }
+};
