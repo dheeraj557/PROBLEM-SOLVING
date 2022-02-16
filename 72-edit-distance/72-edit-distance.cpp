4572744be4c48c70e3314f7f6ad42a682ff72083
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-02-16 23:45:48 +0530

    Time: 12 ms (74.53%), Space: 7.3 MB (83.10%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -16,7 +16,7 @@ public:
                 else if (str1[i - 1] == str2[j - 1])
                     dp[i][j] = dp[i - 1][j - 1];
                 else
-                    dp[i][j]= 1+minFind(dp[i][j - 1],dp[i - 1][j],dp[i - 1][j - 1]);
+                    dp[i][j] = 1+minFind(dp[i][j - 1],dp[i - 1][j],dp[i - 1][j - 1]);
             }
         }
         return dp[m][n];
