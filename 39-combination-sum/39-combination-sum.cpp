ecf60ed360eadee30cebe96ffdba3b0bc03f5a36
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-02-17 10:03:45 +0530

    Time: 3 ms (96.44%), Space: 10.8 MB (88.89%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void findcombination(int ind,int target,vector<int> &arr,vector<vector<int>>&ans,vector<int>&ds)     {
+    void findcombination(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds)     {
 		if(ind==arr.size()){
 			if(target==0){
 				ans.push_back(ds);
