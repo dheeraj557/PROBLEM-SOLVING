5761f037b2d739cc573ee64573d8a0424b8bee6f
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-02-24 14:03:52 +0530

    Time: 26 ms (53.69%), Space: 19.6 MB (52.61%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>m;
+        for(int i:nums)
+        {
+            m[i]++;
+            if(m[i]>nums.size()/2)
+                return i;
+        }
+        return 0;
+    }
+};
