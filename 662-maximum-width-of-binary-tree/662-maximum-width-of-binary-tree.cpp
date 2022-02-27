d22e334bc0a25156016062ac5c927911a4adc5f0
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-02-27 07:45:06 +0530

    Time: 20 ms (20.53%), Space: 17.4 MB (21.15%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-
-unsigned long long ans = 1;
-
-void solve(TreeNode* root,vector<unsigned long long > &v,int l,unsigned long long p){
-    
-    if(root == NULL){
-        return;
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root)
+            return 0;
+        int ans=0;
+        queue<pair<TreeNode* ,int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            int size=q.size();
+            int mini=q.front().second;
+            int first,last;
+            for(int i=0;i<size;i++){
+                int id=q.front().second-mini;
+                TreeNode* node=q.front().first;
+                q.pop();
+                if(i==0)
+                    first=id;
+                if(i==size-1)
+                    last=id;
+                if(node->left)
+                    q.push({node->left,(long long int)id*2+1});
+                if(node->right)
+                    q.push({node->right,(long long int)id*2+2});
+            }
+            ans=max(ans,last-first+1);
+        }
+        return ans;
     }
-    
-    if(v[l] == -1){
-        v[l] = p;
-    }
-    
-    ans = max(ans,p-v[l]+1);
- 
-    
-    solve(root->left,v,l+1,2*p);
-    solve(root->right,v,l+1,2*p+1);
-}
-int widthOfBinaryTree(TreeNode* root) {
-    if(root == NULL){
-        return 0;
-    }
-    
-    vector<unsigned long long> v(3000,-1);
-    solve(root,v,0,1);
-    
-    return ans;
-}
 };
