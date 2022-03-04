ba801ba60e26e922cbe37636c3c822cdc65448a2
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-03-04 09:03:02 +0530

    Added solution - LeetHub

---
+++ Can Make Triangle - GFG/can-make-triangle.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+class Solution{
+    public:
+    bool check(int a,int b,int c)
+    {
+        if(a+b>c and a+c>b and b+c>a)
+            return true;
+        else
+            return false;
+    }
+    vector<int> canMakeTriangle(vector<int> A, int N)
+    {
+        if(N<3)
+            return {};
+        vector<int>result(N-2,0);
+        for(int i=0;i<N-2;i++)
+        {
+            if(check(A[i],A[i+1],A[i+2])==true)
+                result[i]=1;
+            else
+                result[i]=0;
+        }
+        return result;
+    }
+}; 
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> A(N);
+        for(int i=0;i<N;i++)
+            cin>>A[i];
+        Solution ob;
+        auto ans = ob.canMakeTriangle(A, N);
+        for(int i=0;i<ans.size();i++)
+            cout << ans[i] << " ";
+        cout << endl;
+        
+    }
+
+    return 0; 
+}   // } Driver Code Ends
