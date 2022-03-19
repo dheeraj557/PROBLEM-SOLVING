7982df48b40ce6bcf3e9794c38f4b085d656c86a
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-03-19 07:16:17 +0530

    Time: 1047 ms (5.02%), Space: 212.6 MB (5.10%) - LeetHub

---
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -0,0 +1,41 @@
+class FreqStack {
+public:
+    map<int,int> m;
+    stack<pair<int,int> >st;
+    FreqStack() 
+    {
+        while(st.size())
+            st.pop();
+    }
+    void push(int val)  
+    {
+        m[val]++;
+        if(st.size()==0)
+            st.push(make_pair(val,m[val]));
+        else
+        {
+            stack<pair<int,int> >temp;
+            while(st.size() && st.top().second>m[val]){
+                temp.push(st.top());
+            st.pop();
+        }
+        st.push(make_pair(val,m[val]));
+        while(temp.size())
+        {
+            st.push(temp.top());
+            temp.pop();
+        }
+    }
+}
+int pop() 
+{
+    if(st.size()){
+        pair<int,int> ans=st.top();
+        st.pop();
+        m[ans.first]--;
+        return ans.first;
+    }
+    return -1;
+}
+};
+
