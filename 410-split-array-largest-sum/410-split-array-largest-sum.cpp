baaf1f48dc48f84e37bdf634ea4dcf1a8659d449
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-03-31 08:33:28 +0530

    Time: 2 ms (71.63%), Space: 7.1 MB (95.65%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isCapable(vector<int>& v, int m, int total)
+    {
+        int splits = 0, sum = 0;
+        for(auto it : v)
+        {
+            if(it > total) return false;
+            if(sum+it > total)
+            {
+                splits++;
+                sum = it;
+            } 
+            else 
+            {
+                sum += it;
+            }
+        }   
+        return splits < m;
+    }
+    int splitArray(vector<int>& v, int m) {
+        
+        int l = 0, r = INT_MAX;
+        int mid, ans;
+        while(l <= r) 
+        {
+            mid = l + (r-l)/2;
+            if(isCapable(v, m, mid)) 
+            {    
+                ans = mid;
+                r = mid-1;
+            } 
+            else 
+            {
+                l = mid+1;
+            }
+        }      
+        return ans;
+    }
+};
