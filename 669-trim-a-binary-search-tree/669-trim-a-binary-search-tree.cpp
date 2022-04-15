473864ba3ea91d9f6ca55bc853be7aa5099828ea
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-04-15 13:03:44 +0530

    Time: 19 ms (66.98%), Space: 17.4 MB (86.23%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -6,14 +6,12 @@ public:
         if(root->val<low)
         {
             root->left=NULL;
-            root=root->right;
-            root=trimBST(root,low,high);
+            root=trimBST(root->right,low,high);
         }
         else if(root->val>high)
         {
             root->right=NULL;
-            root=root->left;
-            root=trimBST(root,low,high);
+            root=trimBST(root->left,low,high);
         }
         else
         {
