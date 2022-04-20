3e1abad556041413a9de39220945428d45c347f2
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-04-20 09:18:56 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (35.60%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+vector<string> output_final;
+string letters(int num)
+{
+    string result;
+    switch(num)
+    {
+        case 2: result = "abc";
+                break;
+        case 3: result = "def";
+                break;
+        case 4: result = "ghi";
+                break;
+        case 5: result = "jkl";
+                break;
+        case 6: result = "mno";
+                break;
+        case 7: result = "pqrs";
+                break;
+        case 8: result = "tuv";
+                break;
+        case 9: result = "wxyz";
+                break;
+     }
+    return result;
+}
+void helper(int num, string output)
+{
+    if(num == 0)
+    {
+        output_final.push_back(output);
+        return;
+    }
+    else
+    {
+        int last_digit = num % 10;
+        string temp = letters(last_digit);
+        num = num / 10;
+        for(int i = 0; i < temp.length(); i++)
+        {
+            helper(num, temp[i] + output);
+        }
+    }
+}
+
+vector<string> letterCombinations(string digits) 
+{
+    if(digits.length() == 0)
+        return {};
+    string output = "";
+    helper(stoi(digits),output);
+    return output_final;
+}
+};
