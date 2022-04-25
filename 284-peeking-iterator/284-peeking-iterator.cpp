7493473882b6fb7447b310aaf340f0f5b4f75674
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-04-25 09:03:27 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (98.90%) - LeetHub

---
+++ 284-peeking-iterator/284-peeking-iterator.cpp
@@ -0,0 +1,28 @@
+class PeekingIterator : public Iterator {
+    private:
+    
+        int *head ;
+        int pointer = 0 ,size; 
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) 
+    {
+        int n = nums.size();
+	    head = new int[n];
+        for(auto c : nums) 
+            head[pointer++] = c ;
+        pointer = 0 ;
+        size = nums.size();    
+	}
+	int peek() 
+    {
+        return head[pointer] ;
+	}
+	int next() 
+    {
+	    return head[pointer++];
+	}
+	bool hasNext() const 
+    {
+	    return pointer<size ;
+	}
+};
