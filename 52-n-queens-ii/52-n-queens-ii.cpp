5dbce7874689c1c3048640588d3026319b33e861
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-05-09 15:32:01 +0530

    Time: 11 ms (36.43%), Space: 6.8 MB (29.78%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<string>> res;
+    bool is_Safe(vector<string>& board , int row , int column)
+    {
+        for(int index = row ; index >= 0 ; index--)
+            if(board[index][column] == 'Q')
+                return false;
+        for(int index = row , jindex = column ; index >= 0 && jindex >=0 ; index-- , jindex--)
+            if(board[index][jindex] == 'Q')
+                return false;
+        for(int index = row , jindex = column ; index >= 0 && jindex < board.size() ; index-- , jindex++)
+            if(board[index][jindex] == 'Q')
+                return false;   
+        return true;
+    }
+    void placequeen(vector<string>& board , int row)
+    {
+        if(row == board.size())
+        {
+            res.push_back(board);
+            return;
+        }   
+        for(int index = 0 ; index < board.size() ; index++)
+        {
+            if(is_Safe(board , row , index))
+            {
+                
+                board[row][index] = 'Q';
+                placequeen(board , row + 1);
+                board[row][index] = '.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+         if(n <= 0)
+            return res.size();
+        vector<string> board(n,string(n,'.'));
+        placequeen(board,0);
+        return res.size();
+    }
+};
