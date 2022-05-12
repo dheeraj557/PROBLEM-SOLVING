a03fe1cbff66d927d2e48d0e146e99999027dc74
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-05-12 08:13:15 +0530

    Time: 101 ms (16.81%), Space: 9.7 MB (44.45%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     void func(vector<int>& nums, int l,int r, set<vector<int>> &interim) {
-		if (l == r) {
+		if (l == r) 
+        {
 			interim.insert(nums);
 			return;
 		}
-		for (int i = l; i <= r; i++) {
+		for (int i=l;i<=r;i++)
+        {
 			swap(nums[i], nums[l]);
 			func(nums,l + 1, r, interim);
 			swap(nums[i], nums[l]);
 		}
 	}
-	vector<vector<int>> permuteUnique(vector<int>& nums) {
+	vector<vector<int>> permuteUnique(vector<int>& nums) 
+    {
 		set<vector<int>> interim;
 		vector<vector<int>> ans;
 		func(nums, 0,nums.size()-1,interim);
