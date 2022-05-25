b68fb62d0d5650c50d20842e9a1d1b1f2b9fa0de
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-05-25 13:00:43 +0530

    Time: 951 ms (5.13%), Space: 98 MB (5.05%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -7,6 +7,10 @@ public:
     }
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(),envelopes.end(),cmp);
+        for(auto i:envelopes)
+        {
+            cout<<i[0]<<" "<<i[1]<<endl;
+        }
         vector<int>v;
         for(auto i:envelopes){
             auto it=lower_bound(v.begin(),v.end(),i[1]);
@@ -16,8 +20,7 @@ public:
             else
                 *it=i[1];
 			//if it less than or equal to a particular element replace it,because it may form the Longest Increasing subsequence
-            
         }
-        return v.size();
+        return v.size();        
     }
 };
