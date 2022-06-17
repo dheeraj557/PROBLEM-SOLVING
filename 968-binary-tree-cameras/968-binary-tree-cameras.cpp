34e22f7f7a15ca4492ecb25ad1a825edc7d5674c
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-06-17 20:52:00 +0530

    Time: 16 ms (57.04%), Space: 21.3 MB (23.37%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -1,25 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+class Solution
+{
 public:
     int count = 0;
-    int search(TreeNode* root) {
+    int search(TreeNode* root)
+    {
         if(!root)
            return 1;
         int  a = search(root->left);
         int  b = search(root->right);
-        if(a==0 || b==0){
+        if(a==0 || b==0)
+        {
             count++;
-            return 2;}
+            return 2;
+        }
         if(a==1 && b == 1)
             return 0;
         return 1;
