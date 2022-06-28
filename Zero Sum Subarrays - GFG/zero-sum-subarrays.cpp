96653424c612b00e796d9e538f380ca3e317a040
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-06-28 11:11:48 +0530

    Added solution - LeetHub

--- Zero Sum Subarrays - GFG/zero-sum-subarrays.cpp
+++ Zero Sum Subarrays - GFG/zero-sum-subarrays.cpp
@@ -10,45 +10,25 @@ using namespace std;
 
 class Solution{
     public:
-    //Function to count subarrays with sum equal to 0.
     ll findSubarray(vector<ll> arr, int n ) 
     {
         unordered_map<int, vector<int> > map;
- 
-    // create an empty vector of pairs to store
-    // subarray starting and ending index
-    vector <pair<int, int>> out;
- 
-    // Maintains sum of elements so far
-    int sum = 0;
- 
-    for (int i = 0; i < n; i++)
-    {
-        // add current element to sum
-        sum += arr[i];
- 
-        // if sum is 0, we found a subarray starting
-        // from index 0 and ending at index i
-        if (sum == 0)
-            out.push_back(make_pair(0, i));
- 
-        // If sum already exists in the map there exists
-        // at-least one subarray ending at index i with
-        // 0 sum
-        if (map.find(sum) != map.end())
+        ll out=0;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
         {
-            // map[sum] stores starting index of all subarrays
-            vector<int> vc = map[sum];
-            for (auto it = vc.begin(); it != vc.end(); it++)
-                out.push_back(make_pair(*it + 1, i));
+            sum += arr[i];
+            if (sum == 0)
+                out++;
+            if (map.find(sum) != map.end())
+            {
+                vector<int> vc = map[sum];
+                for (auto it = vc.begin(); it != vc.end(); it++)
+                    out++;
+            }
+            map[sum].push_back(i);
         }
- 
-        // Important - no else
-        map[sum].push_back(i);
-    }
- 
-    // return output vector
-    return out.size();
+        return out;
     }
 };
 
