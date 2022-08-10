750d6f7223190b7eb6efcfadba9744af1f47786d
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-08-10 23:56:23 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (40.45%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+public:
+string reverseWords(string s) {
+    string k="";
+    vector<string>v;
+ for(int i=0;i<s.size();i++){
+     if(s[i]!=' '){
+         k+=s[i];
+     }
+     if(s[i]==' ' ||i==s.size()-1){
+        if(k.size()!=0){
+          v.push_back(k);
+            k.clear();
+        } 
+     }
+ }
+    k.clear();
+   for(int i=v.size()-1;i>=0;i--){
+      k+=v[i];
+       if(i!=0){
+           k.push_back(' ');
+       }
+       
+   }
+    return k;
+}
+};
