3866a54e93e561953715e70875f9674b880e1987
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-08-19 07:53:18 +0530

    Time: 308 ms (62.67%), Space: 78.4 MB (63.73%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
+    //storing value in the map to get the freq of the element of the array
     int minSetSize(vector<int>& arr) {
-        int n=arr.size();
-        vector<int>a(n+1); 
-        unordered_map<int,int>m;
-        for(int i=0;i<n;i++)
-        {
+        unordered_map<int,int> m;
+        for(int i=0;i<arr.size();i++){
             m[arr[i]]++;
-            a[m[arr[i]]]++;  
-            a[m[arr[i]]-1]--;
         }
-        int sum =n/2;
-        int ans=0;
-        for(int i=n;i>0;i--)
-        {
-            if(a[i]!=0)
-            {
-                ans+=min(a[i],sum/i+(sum%i==0?0:1));  
-                sum-=(a[i]*i);   
-            }
-            if(sum<=0)
-            {
-                   return ans;
-            }            
-      }
-      return ans;
-   } 
+        //storing the freq in vector so that we can easily access highest freq by sorting the  vector
+        vector<int> freq;
+        int sum=0;
+        for(auto x:m){
+            freq.push_back(x.second);
+            sum+=x.second;
+        }
+       int size=sum;
+       int count=0;
+    sort(freq.begin(),freq.end());
+        
+        //we are using loop till at least half of the integers of the array are removed.
+        while(size>(sum/2)){
+            size=size-freq.back();
+           freq.pop_back();
+        //we are storing the count of element that are removed
+             count++;
+            
+        }
+       
+        return count;
+    }
 };
