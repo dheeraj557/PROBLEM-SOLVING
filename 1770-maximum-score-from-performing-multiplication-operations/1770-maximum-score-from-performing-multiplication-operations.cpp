fe0fced2ad3a83b57b267e78116afa660cc93ad1
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2022-09-16 10:48:41 +0530

    Time: 1194 ms (6.42%), Space: 187.9 MB (11.08%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,25 @@
+typedef long long ll;
+
+class Solution {
+public:
+    int maximumScore(vector<int>& Nums, vector<int>& Mul) {
+        int N = Nums.size(), M = Mul.size();
+        ll Ans = -1e12;
+        vector<vector<ll> > Dp(M + 1, vector<ll>(M + 1, -1e12));
+        Dp[0][0] = 0;        
+        for (int i = 0; i < M; i++) 
+        {
+            int Left = 0, Right = i;
+            while (Right >= 0) 
+            {
+                Dp[Left + 1][Right] = max(Mul[i] * Nums[Left] + Dp[Left][Right], Dp[Left + 1][Right]);
+                Dp[Left][Right + 1] = max(Mul[i] * Nums[N - Right - 1] + Dp[Left][Right], Dp[Left][Right + 1]);     
+                if (i == M - 1) 
+                    Ans = max({Dp[Left + 1][Right], Dp[Left][Right + 1], Ans});       
+                Left++;
+                Right--;
+            }
+        }      
+        return Ans;
+    }
+};
