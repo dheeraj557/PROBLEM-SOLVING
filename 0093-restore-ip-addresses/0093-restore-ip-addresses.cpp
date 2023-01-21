5381b128eb3c86d6b8d309c509e5c51360a9fc83
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-01-21 08:28:38 +0530

    Time: 5 ms (42.51%), Space: 6.6 MB (70.83%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string>IPs;
+        vector<string>temp;
+        getIPAddresses(temp,IPs,s,0);
+        return IPs;
+    }
+    void getIPAddresses(vector<string>&temp,vector<string>&IPs,string &s,int index)
+    {
+        if(index==s.length() and temp.size()==4)
+        {
+            string validIP=temp[0]+'.'+temp[1]+'.'+temp[2]+'.'+temp[3];
+            IPs.push_back(validIP);
+            return;
+        }
+        string tempStr="";
+        for(int i=index;i<s.length() and temp.size()<4;i++)
+        {
+            if(s[i]-'0'>9)
+                continue;
+            tempStr+=s[i];
+            int tempInt=stoi(tempStr);
+            if(0<=tempInt and tempInt<=255)
+            {
+                temp.push_back(tempStr);
+                getIPAddresses(temp,IPs,s,i+1);
+                temp.pop_back();
+            }
+            if(tempInt<=0 or tempInt>255)
+                break;
+        }
+    }
+};
