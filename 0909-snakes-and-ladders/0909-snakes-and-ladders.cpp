0915ea4d7972bc13f63dc9471026a7cde144c3fb
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-01-24 23:38:30 +0530

    Time: 110 ms (13.73%), Space: 24.8 MB (8.37%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:  
-      vector<int> findcordinates(int pos,int n){
+      vector<int> findcordinates(int pos,int n)
+      {
           vector<int>temp;
             int r=n-(pos-1)/n-1;
             int c=(pos-1)%n;
-            if(r%2==n%2){
+            if(r%2==n%2)
+            {
                 return temp={r,n-1-c};
-            }else{
+            }
+            else
+            {
                 return temp={r,c};
             }
-            }
-
-
+    }
     int snakesAndLadders(vector<vector<int>>& board) {
         int n= board.size();
         queue<int>q;
@@ -19,22 +21,30 @@ public:
         int ans=0;
         q.push(1);
         visited[n-1][0]=true;
-        while(!q.empty()){
+        while(!q.empty())
+        {
             int size=q.size();
-            for(int i=0;i<size;++i){
+            for(int i=0;i<size;++i)
+            {
                 int front=q.front();
                 q.pop();
-                if(front==n*n) return ans;
-                for(int k=1;k<=6;++k){
+                if(front==n*n)
+                    return ans;
+                for(int k=1;k<=6;++k)
+                {
                     if(k+front>n*n) break;
                     vector<int>pos=findcordinates(front+k,n);
                     int r=pos[0];
                     int c=pos[1];
-                    if(visited[r][c]==true) continue;
+                    if(visited[r][c]==true) 
+                        continue;
                     visited[r][c]=true;
-                    if(board[r][c]!=-1){
+                    if(board[r][c]!=-1)
+                    {
                         q.push(board[r][c]);
-                    }else{
+                    }
+                    else
+                    {
                         q.push(front+k);
                     }
 
