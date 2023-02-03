034a9ded94b3056f75a5ce0c9a8724e9d3dd23df
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-02-03 09:49:34 +0530

    Time: 882 ms (13.92%), Space: 694 MB (5.00%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    bool repeatedSubstringPattern(string s) {
+    bool repeatedSubstringPattern(string s) 
+    {
         int n=s.size();
         string mix=s+s;
         for(int i=1;i<n;i++)
         {
             string str=mix.substr(i,n);
-            if(str==s) 
+            if(str==s)
                 return true;
         }
         return false;
