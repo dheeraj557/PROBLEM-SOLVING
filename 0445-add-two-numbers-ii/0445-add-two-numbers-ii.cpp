84e87550a95c2c25bcafee2dcb52bc236b844cd6
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-02-15 14:06:25 +0530

    Time: 53 ms (23.98%), Space: 71.2 MB (69.22%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -1,8 +1,11 @@
-class Solution {
+class Solution
+{
 public:
-    ListNode* reverseList(ListNode* curr) {
+    ListNode* reverseList(ListNode* curr)
+    {
         ListNode *prev = nullptr, *next = curr;
-        while(curr) {
+        while(curr)
+        {
             next = curr -> next;
             curr -> next = prev;
             prev = curr;
@@ -10,31 +13,33 @@ public:
         }
         return prev;
     }
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+    {
         ListNode* a = reverseList(l1);
         ListNode* b = reverseList(l2);
-
         ListNode *answer = nullptr, *curr = nullptr;
-
         int carry = 0;
-        while(a && b) {
+        while(a && b)
+        {
             int val = a->val + b->val + carry;
             carry = val / 10;
             val = val % 10;
             ListNode* newNode = new ListNode(val);
-
-            if(!answer) {
+            if(!answer) 
+            {
                 answer = newNode;
                 curr = newNode;
-            }else {
+            }
+            else
+            {
                 curr -> next = newNode;
                 curr = newNode;
             }
             a = a->next;
             b = b->next;
         }
-
-        while(a) {
+        while(a)
+        {
             int val = a->val + carry;
             carry = val / 10;
             val = val % 10;
@@ -43,8 +48,8 @@ public:
             curr = newNode;
             a = a -> next;
         }
-
-        while(b) {
+        while(b)
+        {
             int val = b->val + carry;
             carry = val / 10;
             val = val % 10;
@@ -53,9 +58,7 @@ public:
             curr = newNode;
             b = b -> next;
         }
-
         if(carry) curr -> next = new ListNode(carry);
-
         return reverseList(answer);
     }
 };
