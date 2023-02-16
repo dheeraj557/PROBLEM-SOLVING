cacf9bd92fe49ebbadc752aef038dc50d55bf561
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-02-16 19:53:51 +0530

    Time: 28 ms (79.32%), Space: 24.2 MB (63.78%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -0,0 +1,23 @@
+class BSTIterator {
+public:
+    vector<int>ans;
+    int pos;
+    void inorder(TreeNode* root)
+    {
+        if(root==nullptr)
+            return;
+        inorder(root->left);
+        ans.push_back(root->val);
+        inorder(root->right);
+    }
+    BSTIterator(TreeNode* root) {
+        pos=0;
+        inorder(root);
+    }
+    int next() {
+        return ans[pos++];
+    }
+    bool hasNext() {
+        return ans.size()!=pos;
+    }
+};
