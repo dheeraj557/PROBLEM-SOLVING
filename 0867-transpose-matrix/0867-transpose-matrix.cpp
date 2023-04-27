d9f160659d34b054993285e6904cf1850936ef03
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-04-27 10:43:17 +0530

    Time: 8 ms (88.24%), Space: 10.7 MB (23.98%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-		//Creating auxiliary matrix of size nXm... n=matrix[0].size and m=matrix.size()
-        vector<vector<int>> res(matrix[0].size(), vector<int>(matrix.size()));
-        
-        for(int i=0;i<matrix.size();i++) {
-            for(int j=0;j<matrix[0].size();j++) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> res(m,vector<int> (n,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 res[j][i] = matrix[i][j];
             }
         }
-        
         return res;
     }
 };
