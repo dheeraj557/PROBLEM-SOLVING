05d65215dd36c3f1773a95aebc01e2011030d80c
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-05-03 22:52:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.21%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string ans;    
+        while(n--){
+            ans.push_back(n % 26 + 'A');
+            n /= 26;
+        }
+        reverse(ans.begin() , ans.end());
+        return ans;
+    }
+};
