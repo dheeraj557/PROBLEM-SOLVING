69edf7c857f8c4368b069bbacd871b7d4ac96218
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-05-04 11:53:52 +0530

    Time: 8 ms (60.43%), Space: 6 MB (63.13%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-
-int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2)
-{
-    int a = (ax2 - ax1) * (ay2 - ay1);     
-    int b = (bx2 - bx1) * (by2 - by1);               
-    if (ax1 >= bx2 || ax2 <= bx1 || ay2 <= by1 || ay1 >= by2) 
-        return a + b;                                                 
-    return a + b - (min(bx2, ax2) - max(ax1, bx1)) * (min(ay2, by2) - max(ay1, by1)); 
-}
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int A1 = 0 , A2 = 0 , CA=0;
+        A1 = (ax2-ax1)*(ay2-ay1);
+        A2 = (bx2-bx1)*(by2-by1);
+        if(ax2<bx1 || bx2<ax1 || by2<ay1 || ay2<by1){
+            return A1+A2;
+        }
+        else {
+            int cl = min(bx2 , ax2) - max(bx1,ax1);
+            int cb = min(ay2 , by2) - max(by1,ay1);
+            CA = cl*cb;
+        }
+        return A1+A2-CA;
+    }
 };
