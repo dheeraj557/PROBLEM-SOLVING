f4e9c11359134b4e8df61dc117e34a747320f7e3
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-05-09 18:25:37 +0530

    Time: 13 ms (12.67%), Space: 13.5 MB (98.95%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if(!nums.size()) 
+            return {-1,-1};
+        int low = 0, high = nums.size()-1;        
+        int firstPos = -1, lastPos = -1;
+        while(low < high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] < target) 
+                low = mid+1;
+            else 
+                high = mid;
+        }        
+        if(nums[low] != target) 
+            return {-1,-1};
+        firstPos = low;
+        high = nums.size()-1;
+        while(low < high){
+            int mid = (low + (high-low)/2) + 1;
+            if(nums[mid] > target) 
+                high = mid-1;
+            else 
+                low = mid;
+        }
+        lastPos = high;
+        return {firstPos, lastPos};
+    }
+};
