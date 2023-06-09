5a87a67e38a63d482e695b435f5cd42c532501bb
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-06-09 22:04:32 +0530

    Time: 6 ms (13.00%), Space: 6.1 MB (91.00%) - LeetHub

--- 0483-smallest-good-base/0483-smallest-good-base.cpp
+++ 0483-smallest-good-base/0483-smallest-good-base.cpp
@@ -6,21 +6,14 @@ public:
         ll num = stoll(n);
         for(int i=61; i>=0; i--)
         {
-            ll l=2, r=1e18;
+            ll l=2, r=num-1;
             while(l<=r)
             {
                 ll res=1, pwr=1, j=0, mid=(l+r)/2;
-				
-				// let x be the good base of num
-				// 1 + x + x^2 + x^3 + ... + x^(m-1) = num
-				// |_______________________________|  
-				//                  |
-				//  We are calculating this in this while loop.
-                while(j<i)
+				while(j<i)
                 {
                     if(pwr > (num-res)/mid) // same as -> (res + pwr*mid) > num
                         break;
-
                     pwr *= mid;
                     res += pwr;
 
