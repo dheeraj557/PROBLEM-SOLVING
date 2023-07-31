70c469d1d04711ad9c30112f8de43d479d21418c
Dheeraj Chandwani <65399728+dheeraj557@users.noreply.github.com>
2023-07-31 23:55:07 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (34.93%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        if (numRows == 0)
-            return {};
-        
-        vector<vector<int>> res;
-        res.push_back({1}); // First row: 1
-        
-        for (int i = 1; i < numRows; i++) {
-            vector<int> temp(i + 1, 1); // Start with i+1 elements, all initialized to 1
-            
-            for (int j = 1; j < i; j++) {
-                temp[j] = res[i - 1][j - 1] + res[i - 1][j]; // Compute middle elements of the row
+        if(numRows==0)
+            return {{}};
+        if(numRows==1)
+            return {{1}};
+        if(numRows==2)
+            return {{1},{1,1}};
+        vector<vector<int>>res;
+        res.push_back({1});
+        res.push_back({1,1});
+        for(int i=3;i<=numRows;i++)
+        {
+            vector<int>temp(i);
+            temp[0]=1;
+            temp[i-1]=1;
+            for(int j=1;j<i-1;j++)
+            {
+                temp[j]=res[i-2][j-1]+res[i-2][j];
             }
-            
-            res.push_back(temp); // Add the generated row to the result
+            res.push_back(temp);
         }
-        
         return res;
     }
 };
